1df576625c2f25aee05b3642b40a6f899da396e0
Bengt Bengtsson <bengt.bengtsson@gmail.com>
2024-06-30 13:56:49 +0200

    implemented redux toolkit demo

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { store } from './src/app/store'
+import { Provider } from 'react-redux'
+import Counter from './src/features/counter/Counter'
 
 export default function App() {
   return (
+    <Provider store={store}>
     <View style={styles.container}>
+      <Counter />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    </Provider>
   );
 }
 
